af5d467d2b52e44dce361945a9f4ede68c7475af
Brandy <brandyraquel323@gmail.com>
2021-12-12 19:43:29 -0500

    commit

--- 03-JavaScript/02-Challenge/Develop/script.js
+++ 03-JavaScript/02-Challenge/Develop/script.js
@@ -22,6 +22,9 @@ console.log(promptLower)
 var promptNumbers = window.prompt ("Would you like to include numbers? Enter 'YES or CANCEL'");
 console.log(promptNumbers);
 
+if (charLength<8){
+  window.alert("Please enter minimum of 8. Please follow all criteria")
+})
 
 
 
