bf0fea44df543d734646889955059f4a790a69fc
Brandy <brandyraquel323@gmail.com>
2021-12-12 19:25:07 -0500

    added criteria

--- 03-JavaScript/02-Challenge/Develop/script.js
+++ 03-JavaScript/02-Challenge/Develop/script.js
@@ -5,7 +5,31 @@ generateBtn.onclick = function() {
 
 
 promptCharLength = window.alert("Password requires minimum of 8 characters and no more than 128 characters");
+
+
+Criteria = window.alert("Password includes combination of special characters, lenght of characters, numbers, uppercase letters, lowercase letters. Pick password Criteria on next prompt");
+window.alert("Choose password Criteria");
+
+var charLength = window.prompt("Enter number of characters between 8 and 128")
+console.log(charLength);
+
+var promptUpper = window.prompt ("Would you like to include upper case letters? Enter 'YES or CANCEL'");
+console.log(promptUpper);
+
+var promptLower = window.prompt ("Would you like to include lower case letters? Enter 'YES or CANCEL'");
+console.log(promptLower)
+
+var prompNumbers = window.prompt ("Would you like to include numbers? Enter 'YES or CANCEL'");
+
+
+
+
+
+
+
+
 }
+
 
 
 // Write password to the #password input
